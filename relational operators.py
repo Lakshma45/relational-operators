ecb70cd1203dc56095fb1fbbb332f8cdb8555043
Lakshma45 <111054201+Lakshma45@users.noreply.github.com>
2022-11-24 20:26:29 +0530

    Add files via upload

---
+++ relational operators.py
@@ -0,0 +1,15 @@
+print('enter the values')
+a=int(input())
+b=int(input())
+c=a<b
+print('less than',c)
+d=a>b
+print('greater than',d)
+e=a>=b
+print('greater than or equal to',e)
+g=a<=b
+print('less than or equals to',g)
+f=a==b
+print('eqals to',f)
+q=a!=b
+print('not equals',q)
